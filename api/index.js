225e7f2593982986679724d88580b9f72570e22c
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-07-05 01:16:22 +0200

    connection with database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectToDB from './config/db.js';
+dotenv.config()
 
 const app = express()
-const port = 3000
+connectToDB()
 
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
+app.listen(3000, () => {
+    console.log(`server is listening on port ${3000}`)
 })
