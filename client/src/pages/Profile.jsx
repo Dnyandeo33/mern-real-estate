8557933ef680ff650101890cac412f90e965b9c4
Dnyandeo Waghunde <dnyanu.waghunde@gmail.com>
2024-07-11 10:33:57 +0200

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,81 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { app } from '../firebase';
 
 const Profile = () => {
+  const [file, setFile] = useState(undefined);
+  const [progress, setProgress] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState('');
+  const [formData, setFormData] = useState({});
+
+  const fileRef = useRef();
   const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className=" text-3xl text-center font-semibold my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          hidden
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+        />
         <img
+          onClick={() => fileRef.current.click()}
           className="h-24 w-24 rounded-full object-cover cursor-pointer self-center mt-2"
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt=""
         />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-500">
+              Upload Failed(image must be less then 2mb){' '}
+            </span>
+          ) : progress > 0 && progress < 100 ? (
+            <span className="text-slate-500">{`uploading ${progress}`}</span>
+          ) : progress === 100 ? (
+            <span className="text-green-500">Uploaded successfully...</span>
+          ) : (
+            ''
+          )}
+        </p>
         <input
           className="p-3 border rounded-lg"
           type="text"
